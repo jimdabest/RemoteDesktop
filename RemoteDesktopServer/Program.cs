61d3e0451b6710a6c02bd2ebd195eb5af44fd5e4
PhamNguyen <jim.phamnguyen@gmail.com>
2026-06-05 15:59:02 +0700

    Design Server UI

--- RemoteDesktopServer/Program.cs
+++ RemoteDesktopServer/Program.cs
@@ -16,7 +16,7 @@ namespace RemoteDesktopServer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmHost());
         }
     }
 }
